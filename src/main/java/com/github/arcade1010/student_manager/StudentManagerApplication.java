ca22cddca2208c34f53ac5f393b3f12c897d2940
arcade1010 <enwurster@gmail.com>
2025-02-12 16:11:14 -0500

    Initial commit

---
+++ src/main/java/com/github/arcade1010/student_manager/StudentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.arcade1010.student_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagerApplication.class, args);
+	}
+
+}
